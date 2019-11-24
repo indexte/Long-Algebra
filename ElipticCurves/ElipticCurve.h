291c71bd4f87b1cc0979c156479b37c5d060dfac
Nikita Pupov <nikitospupov@gmail.com>
2019-11-24 07:30:25 +0200

    Modified ElipticCurves,so it doesn't need N in BigNumber inputs. Changed N.getN() to N.to_string(). Added getter to N

--- ElipticCurves/ElipticCurve.h
+++ ElipticCurves/ElipticCurve.h
@@ -9,16 +9,23 @@ private:
 	BigNumber A;
 	BigNumber B;
 	BigNumber N;
+
+	Point getPointWithN(Point point);
 public:
 	ElipticCurve(BigNumber A, BigNumber B, BigNumber N):A(A),B(B),N(N) {
-		BigNumber discriminant = BigNumber("16",N.getN()) * (BigNumber("4",N.getN()) * A*A*A + BigNumber("27",N.getN()) * B*B);
-		if (discriminant == BigNumber("0",N.getN())) {
+		//set N in case user haven't done it
+		this->A.setN(N.to_string());
+		this->B.setN(N.to_string());
+
+		BigNumber discriminant = BigNumber("16",N.to_string()) * (BigNumber("4",N.to_string()) * A*A*A + BigNumber("27",N.to_string()) * B*B);
+		if (discriminant == BigNumber("0",N.to_string())) {
 			throw new std::invalid_argument("Discriminant cannot be equal 0");
 		}
 	}
 
 	BigNumber getA();
 	BigNumber getB();
+	BigNumber getN();
 
 	Point addPoints(Point a, Point b);
 	Point getInversePoint(Point a);
@@ -34,8 +41,19 @@ BigNumber ElipticCurve::getA() {
 	return A;
 }
 
-//what if we have negative? 
+BigNumber ElipticCurve::getN() {
+	return N;
+}
+
+/**
+*@brief adds two points a and b
+*@return sum of two points a and b.
+*@throw invalid_argument if one of the points is not on the curve
+*/
 Point ElipticCurve::addPoints(Point a, Point b) {
+	a = getPointWithN(a);
+	b = getPointWithN(b);
+
 	if (a.isInfinitePoint())
 		return b;
 
@@ -51,10 +69,10 @@ Point ElipticCurve::addPoints(Point a, Point b) {
 		return Point::getInfinitePoint();
 	}
 
-	BigNumber lambda("0",N.getN());
+	BigNumber lambda("0",N.to_string());
 	if (a.getX() == b.getX()) {
 		//lambda=(3*a^2+A)/(2*y1) mod N
-		lambda = (BigNumber("3",N.getN()) * a.getX()*a.getX() + A)/ (BigNumber("2",N.getN()) * a.getY());
+		lambda = (BigNumber("3",N.to_string()) * a.getX()*a.getX() + A)/ (BigNumber("2",N.to_string()) * a.getY());
 	}
 	else {
 		//lambda=(y2-y1)/(x2-x1) mod N
@@ -69,7 +87,13 @@ Point ElipticCurve::addPoints(Point a, Point b) {
 	return Point(x, y);
 }
 
+/**
+*@return inverse point
+*@throw invalid_argument if point is not on the curve
+*/
 Point ElipticCurve::getInversePoint(Point a) {
+	a = getPointWithN(a);
+
 	if (!isPointOnCurve(a)) {
 		throw new std::invalid_argument("Some of the points are not on the curve");
 	}
@@ -77,12 +101,31 @@ Point ElipticCurve::getInversePoint(Point a) {
 	return Point(a.getX(),-a.getY());
 }
 
+/**
+*@brief checks if point is on curve
+*@return true if point is on the curve, otherwise false
+*/
 bool ElipticCurve::isPointOnCurve(Point a) {
+	a = getPointWithN(a);
+
 	BigNumber k = a.getY()*a.getY();
 	if (a.getY()*a.getY() != a.getX()*a.getX()*a.getX() + A * a.getX() + B)
 		return false;
-	if (BigNumber("0",N.getN())> a.getX() || a.getX() > N)
+	if (a.getX() >= N || a.getY()>=N)
 		return false;
 
 	return true;
+}
+
+/**
+*@brief sets N to the coordinates of the point
+*@return point with correct N
+*This function is created to allow users pass points without setting N to their coordinates.
+*/
+Point ElipticCurve::getPointWithN(Point point) {
+	BigNumber x = point.getX();
+	x.setN(N.to_string());
+	BigNumber y = point.getY();
+	y.setN(N.to_string());
+	return Point(x, y);
 }
--- ElipticCurves/Source.cpp
+++ ElipticCurves/Source.cpp
@@ -10,11 +10,11 @@ using namespace std;
 //https://studref.com/403745/informatika/algoritmy_osnove_ellipticheskih_krivyh
 //https://people.cs.nctu.edu.tw/~rjchen/ECC2012S/Elliptic%20Curves%20Number%20Theory%20And%20Cryptography%202n.pdf
 int main() {
-	Point a = Point(BigNumber("13","23"), BigNumber("7","23"));
-	Point b = Point(BigNumber("3", "23"), BigNumber("10", "23"));
+	Point a = Point(BigNumber("13"), BigNumber("7"));
+	Point b = Point(BigNumber("3"), BigNumber("10"));
 
 	//A=1,B=1,N=23
-	ElipticCurve curve = ElipticCurve(BigNumber("1","23"), BigNumber("1","23"), BigNumber("23","23"));
+	ElipticCurve curve = ElipticCurve(BigNumber("1"), BigNumber("1"), BigNumber("23"));
 
 	Point sumAB = curve.addPoints(a, b);
 
